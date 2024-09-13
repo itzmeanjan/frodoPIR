657b5a9b3cc19966a5cf75cee08d0899bf834059
Anjan Roy <hello@itzmeanjan.in>
2024-09-13 19:40:21 +0400

    Serialization and deserialization of matrix data type
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/frodoPIR/internals/matrix/matrix.hpp
+++ include/frodoPIR/internals/matrix/matrix.hpp
@@ -42,21 +42,25 @@ public:
     return mat;
   }
 
-  // Accessor(s)
-  forceinline constexpr zq_t& operator[](std::pair<size_t, size_t> idx)
+  // Accessor, using {row_index, column_index} pair.
+  forceinline constexpr zq_t& operator[](const std::pair<size_t, size_t> idx)
   {
     const auto [r_idx, c_idx] = idx;
     return this->elements[r_idx * cols + c_idx];
   }
-  forceinline constexpr const zq_t& operator[](std::pair<size_t, size_t> idx) const
+  forceinline constexpr const zq_t& operator[](const std::pair<size_t, size_t> idx) const
   {
     const auto [r_idx, c_idx] = idx;
     return this->elements[r_idx * cols + c_idx];
   }
 
+  // Accessor, using linearized index.
+  forceinline constexpr zq_t& operator[](const size_t lin_idx) { return this->elements[lin_idx]; }
+  forceinline constexpr const zq_t& operator[](const size_t lin_idx) const { return this->elements[lin_idx]; }
+
   // Given two matrices A, B of equal dimension, this routine can be used for performing matrix addition over Zq,
   // returning a matrix of same dimension.
-  inline constexpr matrix_t operator+(const matrix_t<rows, cols>& rhs) const
+  forceinline constexpr matrix_t operator+(const matrix_t<rows, cols>& rhs) const
   {
     matrix_t res{};
 
@@ -85,6 +89,45 @@ public:
 
         res[{ r_idx, c_idx }] = tmp;
       }
+    }
+
+    return res;
+  }
+
+  // Given a matrix M of dimension `rows x cols`, this routine can be used for serializing each of its elements as
+  // four little-endian bytes and concatenating them in order to compute a byte array of length m * n * 4.
+  forceinline constexpr void to_le_bytes(std::span<uint8_t, rows * cols * sizeof(zq_t)> bytes) const
+  {
+    for (size_t i = 0; i < rows * cols; i++) {
+      const size_t boff = i * sizeof(zq_t);
+
+      const auto word = (*this)[i];
+
+      bytes[boff + 0] = static_cast<uint8_t>((word >> 0u) & 0xffu);
+      bytes[boff + 1] = static_cast<uint8_t>((word >> 8u) & 0xffu);
+      bytes[boff + 2] = static_cast<uint8_t>((word >> 16u) & 0xffu);
+      bytes[boff + 3] = static_cast<uint8_t>((word >> 24u) & 0xffu);
+    }
+  }
+
+  // Given a byte array of length m * n * 4, this routine can be used for deserializing it as a matrix of dimension m x n
+  // s.t. each matrix element is computed by interpreting four consecutive bytes in little-endian order.
+  forceinline static matrix_t from_le_bytes(std::span<const uint8_t, rows * cols * sizeof(zq_t)> bytes)
+  {
+    constexpr size_t blen = bytes.size();
+    matrix_t res{};
+
+    size_t boff = 0;
+    size_t lin_idx = 0;
+
+    while (boff < blen) {
+      const zq_t word = ((static_cast<zq_t>(bytes[boff + 3]) << 24u) | (static_cast<zq_t>(bytes[boff + 2]) << 16u) |
+                         (static_cast<zq_t>(bytes[boff + 1]) << 8u) | (static_cast<zq_t>(bytes[boff + 0]) << 0u));
+
+      res[lin_idx] = word;
+
+      boff += sizeof(zq_t);
+      lin_idx += 1;
     }
 
     return res;
