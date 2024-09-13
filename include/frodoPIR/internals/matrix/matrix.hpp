8e5e016298fcadd8b829e18e1862e4868bcaad9e
Anjan Roy <hello@itzmeanjan.in>
2024-09-13 19:20:38 +0400

    Multiply two matrices of valid dimension
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/frodoPIR/internals/matrix/matrix.hpp
+++ include/frodoPIR/internals/matrix/matrix.hpp
@@ -53,6 +53,29 @@ public:
     const auto [r_idx, c_idx] = idx;
     return this->elements[r_idx * cols + c_idx];
   }
+
+  // Given two matrices A ( of dimension rows x cols ) and B ( of dimension rhs_rows x rhs_cols ) s.t. cols == rhs_rows,
+  // this routine can be used for multiplying them over Zq, resulting into another matrix (C) of dimension rows x rhs_cols.
+  template<size_t rhs_rows, size_t rhs_cols>
+    requires((cols == rhs_rows))
+  forceinline constexpr matrix_t<rows, rhs_cols> operator*(const matrix_t<rhs_rows, rhs_cols>& rhs)
+  {
+    matrix_t<rows, rhs_cols> res{};
+
+    for (size_t r_idx = 0; r_idx < rows; r_idx++) {
+      for (size_t c_idx = 0; c_idx < rhs_cols; c_idx++) {
+        zq_t tmp{};
+
+        for (size_t k = 0; k < cols; k++) {
+          tmp += (*this)[{ r_idx, k }] * rhs[{ k, c_idx }];
+        }
+
+        res[{ r_idx, c_idx }] = tmp;
+      }
+    }
+
+    return res;
+  }
 };
 
 }
