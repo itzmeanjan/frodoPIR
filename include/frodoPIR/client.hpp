87098f2c5bc172be179413e716cc911947b5969a
Anjan Roy <hello@itzmeanjan.in>
2024-09-18 23:43:56 +0400

    Add functionality of processing server response at client side
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/frodoPIR/client.hpp
+++ include/frodoPIR/client.hpp
@@ -16,7 +16,6 @@ enum class query_status_t : uint32_t
 {
   prepared,
   sent,
-  responded
 };
 
 // FrodoPIR client query data type
@@ -96,9 +95,53 @@ public:
     constexpr auto ρ = 1ul << mat_element_bitlen;
     constexpr auto query_indicator_value = static_cast<frodoPIR_matrix::zq_t>(q / ρ);
 
-    this->queries[db_row_index].status = query_status_t::sent;
     this->queries[db_row_index].b[db_row_index] += query_indicator_value;
     this->queries[db_row_index].b.to_le_bytes(query_bytes);
+    this->queries[db_row_index].status = query_status_t::sent;
+
+    return true;
+  }
+
+  // Given a database row index, for which query has already been sent to server and we are awaiting response,
+  // this routine can be used for processing server response, returning byte serialized content of queried row.
+  constexpr bool process_response(
+    const size_t db_row_index,
+    std::span<const uint8_t, frodoPIR_matrix::get_required_num_columns(db_entry_byte_len, mat_element_bitlen) * sizeof(frodoPIR_matrix::zq_t)> response_bytes,
+    std::span<uint8_t, db_entry_byte_len> db_row_bytes)
+  {
+    if (!this->queries.contains(db_row_index)) {
+      return false;
+    }
+    if (this->queries[db_row_index].status != query_status_t::sent) {
+      return false;
+    }
+
+    constexpr auto q = static_cast<uint64_t>(std::numeric_limits<frodoPIR_matrix::zq_t>::max()) + 1;
+    constexpr auto rho = 1ul << mat_element_bitlen;
+    constexpr auto rounding_factor = static_cast<frodoPIR_matrix::zq_t>(q / rho);
+    constexpr auto rounding_floor = rounding_factor / 2;
+
+    constexpr size_t db_matrix_row_width = frodoPIR_matrix::get_required_num_columns(db_entry_byte_len, mat_element_bitlen);
+
+    frodoPIR_vector::vector_t<db_matrix_row_width> db_matrix_row{};
+    auto c_tilda = frodoPIR_vector::vector_t<db_matrix_row_width>::from_le_bytes(response_bytes);
+
+    for (size_t idx = 0; idx < db_matrix_row_width; idx++) {
+      const auto unscaled_res = c_tilda[idx] - this->queries[db_row_index].c[idx];
+
+      const auto scaled_res = unscaled_res / rounding_factor;
+      const auto scaled_rem = unscaled_res % rounding_factor;
+
+      auto rounded_res = scaled_res;
+
+      rounded_res += ((scaled_rem > rounding_floor) ? 1 : 0);
+      rounded_res %= static_cast<frodoPIR_matrix::zq_t>(rho);
+
+      db_matrix_row[idx] = rounded_res;
+    }
+
+    db_matrix_row.serialize_db_row<db_entry_byte_len, mat_element_bitlen>(db_row_bytes);
+    this->queries.erase(db_row_index);
 
     return true;
   }
