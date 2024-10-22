cf8c0d6ad602fbeb5654a5886fb35f8e8f2ba82a
Anjan Roy <hello@itzmeanjan.in>
2024-10-22 16:12:28 +0400

    Simplify server implementation - make it easier to read
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/frodoPIR/server.hpp
+++ include/frodoPIR/server.hpp
@@ -17,8 +17,20 @@ template<size_t λ, size_t db_entry_count, size_t db_entry_byte_len, size_t mat_
 struct server_t
 {
 public:
+  // Compile-time computable values.
+  static constexpr auto parsed_db_num_cols = frodoPIR_matrix::get_required_num_columns(db_entry_byte_len, mat_element_bitlen);
+  static constexpr auto orig_db_byte_len = db_entry_count * db_entry_byte_len;
+  static constexpr auto query_byte_len = db_entry_count * sizeof(frodoPIR_matrix::zq_t);
+  static constexpr auto response_byte_len = parsed_db_num_cols * sizeof(frodoPIR_matrix::zq_t);
+
+  // Type aliases.
+  using pub_mat_A_t = frodoPIR_matrix::matrix_t<lwe_dimension, db_entry_count>;
+  using pub_mat_M_t = frodoPIR_matrix::matrix_t<lwe_dimension, parsed_db_num_cols>;
+  using parsed_db_t = frodoPIR_matrix::matrix_t<db_entry_count, parsed_db_num_cols>;
+  using query_t = frodoPIR_vector::row_vector_t<db_entry_count>;
+
   // Constructor(s)
-  explicit constexpr server_t(frodoPIR_matrix::matrix_t<db_entry_count, frodoPIR_matrix::get_required_num_columns(db_entry_byte_len, mat_element_bitlen)> db)
+  explicit constexpr server_t(auto db)
     : D(std::move(db))
   {
   }
@@ -32,11 +44,10 @@ public:
   // each entry is of `db_entry_byte_len` -bytes, this routine can be used for setting up FrodoPIR server,
   // returning initialized server (ready to respond to client queries) handle and public matrix M, which will
   // be used by clients for preprocessing queries.
-  static forceinline constexpr std::
-    pair<server_t, frodoPIR_matrix::matrix_t<lwe_dimension, frodoPIR_matrix::get_required_num_columns(db_entry_byte_len, mat_element_bitlen)>>
-    setup(std::span<const uint8_t, λ / std::numeric_limits<uint8_t>::digits> seed_μ, std::span<const uint8_t, db_entry_count * db_entry_byte_len> db_bytes)
+  static forceinline constexpr std::pair<server_t, pub_mat_M_t> setup(std::span<const uint8_t, λ / std::numeric_limits<uint8_t>::digits> seed_μ,
+                                                                      std::span<const uint8_t, orig_db_byte_len> db_bytes)
   {
-    const auto A = frodoPIR_matrix::matrix_t<lwe_dimension, db_entry_count>::template generate<λ>(seed_μ);
+    const auto A = pub_mat_A_t::template generate<λ>(seed_μ);
     const auto D = frodoPIR_serialization::parse_db_bytes<db_entry_count, db_entry_byte_len, mat_element_bitlen>(db_bytes);
     const auto M = A * D;
 
@@ -44,18 +55,16 @@ public:
   }
 
   // Given byte serialized client query, this routine can be used for responding back to it, producing byte serialized server response.
-  constexpr void respond(
-    std::span<const uint8_t, db_entry_count * sizeof(frodoPIR_matrix::zq_t)> query_bytes,
-    std::span<uint8_t, frodoPIR_matrix::get_required_num_columns(db_entry_byte_len, mat_element_bitlen) * sizeof(frodoPIR_matrix::zq_t)> response_bytes) const
+  constexpr void respond(std::span<const uint8_t, query_byte_len> query_bytes, std::span<uint8_t, response_byte_len> response_bytes) const
   {
-    const auto b_tilda = frodoPIR_vector::row_vector_t<db_entry_count>::from_le_bytes(query_bytes);
+    const auto b_tilda = query_t::from_le_bytes(query_bytes);
     const auto c_tilda = b_tilda * this->D;
 
     c_tilda.to_le_bytes(response_bytes);
   }
 
 private:
-  frodoPIR_matrix::matrix_t<db_entry_count, frodoPIR_matrix::get_required_num_columns(db_entry_byte_len, mat_element_bitlen)> D{};
+  parsed_db_t D{};
 };
 
 }
